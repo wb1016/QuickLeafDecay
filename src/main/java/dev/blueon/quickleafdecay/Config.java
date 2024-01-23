ba9728d5b6c414f4e583ca1fa834c8a383f6f23f
wb1016 <star_tracker@protonmail.com>
2024-01-23 23:09:19 +0900

    remove cloth config implementation and dependency, optimize import, rewrite metadata

--- src/main/java/dev/blueon/quickleafdecay/Config.java
+++ src/main/java/dev/blueon/quickleafdecay/Config.java
@@ -1,47 +1,35 @@
 package dev.blueon.quickleafdecay;
 
-import me.shedaniel.autoconfig.ConfigData;
-import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import dev.blueon.quickleafdecay.configloader.ConfigLoader;
 
-import static dev.blueon.quickleafdecay.QuickLeafDecay.NAMESPACE;
+public class Config
+{
+    public static boolean matchLeavesTypes = true;
+    public static boolean unknownLeavesOnlyMatchSelf = true;
+    public static boolean matchLogsToLeaves = true;
+    public static boolean ignorePersistentLeaves = true;
+    public static boolean accelerateLeavesDecay = true;
+    public static int minDecayDelay = 10;
+	public static int maxDecayDelay = 60;	
+	public static boolean updateDiagonalLeaves = true;
+	public static boolean doDecayingLeavesEffects = false;
+	public static boolean fetchTranslationUpdates = true;
 
-@me.shedaniel.autoconfig.annotation.Config(name = NAMESPACE)
-public class Config implements ConfigData {
-    @ConfigEntry.Gui.Tooltip
-    public boolean matchLeavesTypes = FeatureControl.Defaults.matchLeavesTypes;
+    // Saving and loading
 
-    @ConfigEntry.Gui.Tooltip
-    public boolean unknownLeavesOnlyMatchSelf = FeatureControl.Defaults.unknownLeavesOnlyMatchSelf;
-
-    @ConfigEntry.Gui.Tooltip
-    public boolean matchLogsToLeaves = FeatureControl.Defaults.matchLogsToLeaves;
-
-    @ConfigEntry.Gui.Tooltip
-    public boolean ignorePersistentLeaves = FeatureControl.Defaults.ignorePersistentLeaves;
-
-    @ConfigEntry.Gui.Tooltip
-    public boolean accelerateLeavesDecay = FeatureControl.Defaults.accelerateLeavesDecay;
-
-    @ConfigEntry.Gui.Tooltip
-    @ConfigEntry.Gui.CollapsibleObject
-    public DecayDelay decayDelay = new DecayDelay();
-
-    @ConfigEntry.Gui.Tooltip
-    public boolean updateDiagonalLeaves = FeatureControl.Defaults.updateDiagonalLeaves;
-
-    @ConfigEntry.Gui.Tooltip
-    public boolean doDecayingLeavesEffects = FeatureControl.Defaults.doDecayingLeavesEffects;
-
-    @ConfigEntry.Gui.Tooltip
-    public boolean fetchTranslationUpdates = FeatureControl.Defaults.fetchTranslationUpdates;
-
-    public static class DecayDelay {
-        @ConfigEntry.Gui.Tooltip
-        @ConfigEntry.BoundedDiscrete(max = 100)
-        public int minimum = FeatureControl.Defaults.minDecayDelay;
-
-        @ConfigEntry.BoundedDiscrete(max = 100)
-        @ConfigEntry.Gui.Tooltip
-        public int maximum = FeatureControl.Defaults.maxDecayDelay;
+    public static void load()
+    {
+        ConfigLoader.load(Config.class, getFileName());
     }
+
+    public static void save()
+    {
+        ConfigLoader.save(Config.class, getFileName());
+    }
+
+    private static String getFileName()
+    {
+        return "quickleafdecay.json";
+    }
+
 }
--- src/main/java/dev/blueon/quickleafdecay/FeatureControl.java
+++ src/main/java/dev/blueon/quickleafdecay/FeatureControl.java
@@ -1,103 +1,54 @@
 package dev.blueon.quickleafdecay;
 
-import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
-import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.ModContainer;
-import net.fabricmc.loader.api.VersionParsingException;
-import net.fabricmc.loader.api.metadata.version.VersionPredicate;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.random.RandomGenerator;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Optional;
-
 public final class FeatureControl {
-    private static final Config CONFIG_INSTANCE;
-
-    static {
-        boolean shouldLoadConfig = false;
-
-        final Optional<ModContainer> optModContainer = FabricLoader.getInstance().getModContainer("cloth-config");
-        if (optModContainer.isPresent()) {
-            try {
-                shouldLoadConfig = VersionPredicate.parse(">=7.0.72").test(optModContainer.get().getMetadata().getVersion());
-            } catch (VersionParsingException e) {
-                e.printStackTrace();
-            }
-        }
-        
-        CONFIG_INSTANCE = shouldLoadConfig ?
-                AutoConfig.register(Config.class, GsonConfigSerializer::new).getConfig() : null;
-
-    }
-
-    public static class Defaults {
-        public static final boolean matchLeavesTypes = true;
-        public static final boolean unknownLeavesOnlyMatchSelf = true;
-        public static final boolean matchLogsToLeaves = true;
-        public static final boolean ignorePersistentLeaves = true;
-        public static final boolean accelerateLeavesDecay = true;
-        public static final int minDecayDelay = 10;
-        public static final int maxDecayDelay = 60;
-        public static final boolean updateDiagonalLeaves = true;
-        public static final boolean doDecayingLeavesEffects = false;
-        public static final boolean fetchTranslationUpdates = true;
-
-        private Defaults() { }
-    }
-
-    public static boolean isConfigLoaded() {
-        return CONFIG_INSTANCE != null;
-    }
-
+	
     public static boolean shouldMatchLeavesTypes() {
-        return CONFIG_INSTANCE == null ? Defaults.matchLeavesTypes : CONFIG_INSTANCE.matchLeavesTypes;
+        return Config.matchLeavesTypes;
     }
 
     public static boolean shouldUnknownLeavesOnlyMatchSelf() {
-        return CONFIG_INSTANCE == null ? Defaults.unknownLeavesOnlyMatchSelf : CONFIG_INSTANCE.unknownLeavesOnlyMatchSelf;
+        return Config.unknownLeavesOnlyMatchSelf;
     }
 
     public static boolean shouldMatchLogsToLeaves() {
-        return CONFIG_INSTANCE == null ? Defaults.matchLogsToLeaves : CONFIG_INSTANCE.matchLogsToLeaves;
+        return Config.matchLogsToLeaves;
     }
 
     public static boolean shouldIgnorePersistentLeaves() {
-        return CONFIG_INSTANCE == null ? Defaults.ignorePersistentLeaves : CONFIG_INSTANCE.ignorePersistentLeaves;
+        return Config.ignorePersistentLeaves;
     }
 
     public static boolean shouldAccelerateLeavesDecay() {
-        return CONFIG_INSTANCE == null ? Defaults.accelerateLeavesDecay : CONFIG_INSTANCE.accelerateLeavesDecay;
+        return Config.accelerateLeavesDecay;
     }
 
     public static int getDecayDelay(RandomGenerator random) {
         final int minDecayDelay;
         final int maxDecayDelay;
-        if (CONFIG_INSTANCE == null) {
-            minDecayDelay = Defaults.minDecayDelay;
-            maxDecayDelay = Defaults.maxDecayDelay;
-        } else {
-            minDecayDelay = CONFIG_INSTANCE.decayDelay.minimum;
-            maxDecayDelay = CONFIG_INSTANCE.decayDelay.maximum;
-        }
+        
+        minDecayDelay = Config.minDecayDelay;
+        maxDecayDelay = Config.maxDecayDelay;
 
         return minDecayDelay < maxDecayDelay ?
              random.range(minDecayDelay, maxDecayDelay + 1) : maxDecayDelay;
     }
 
     public static boolean shouldUpdateDiagonalLeaves() {
-        return CONFIG_INSTANCE == null ? Defaults.updateDiagonalLeaves : CONFIG_INSTANCE.updateDiagonalLeaves;
+        return Config.updateDiagonalLeaves;
     }
 
     public static boolean shouldDoDecayingLeavesEffects() {
-        return CONFIG_INSTANCE == null ? Defaults.doDecayingLeavesEffects : CONFIG_INSTANCE.doDecayingLeavesEffects;
+        return Config.doDecayingLeavesEffects;
     }
 
     public static boolean shouldFetchTranslationUpdates() {
-        return CONFIG_INSTANCE == null ? Defaults.fetchTranslationUpdates : CONFIG_INSTANCE.fetchTranslationUpdates;
+        return Config.fetchTranslationUpdates;
     }
 
     public static boolean isMatchingLeaves(TagKey<Block> leavesTag, @NotNull BlockState state, @NotNull BlockState currentLeavesState) {
@@ -106,7 +57,10 @@ public final class FeatureControl {
         else return state.isIn(leavesTag);
     }
     
-    public static void init() { }
+    public static void init() {
+		Config.load();
+        Config.save();
+	}
 
     private FeatureControl() { }
 }
